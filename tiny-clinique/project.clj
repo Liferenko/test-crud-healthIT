afde6e2f08ea2f5abef8adad7966358145ad761a
Павел Лиференко <eleven.krsk@gmail.com>
2020-10-22 17:15:55 +0300

    init project + test env (works ok)

---
+++ tiny-clinique/project.clj
@@ -0,0 +1,11 @@
+(defproject tiny-clinique "0.1.0-SNAPSHOT"
+  :description "tiny app for client management"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"],[expectations/expectations "1.4.5"]]
+  :plugins [[lein-expectations "0.0.8"]]
+  :main ^:skip-aot tiny-clinique.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
