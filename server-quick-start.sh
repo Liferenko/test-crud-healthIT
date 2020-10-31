4d4b0fbe9985a476e5ef3b5d09071051c18e9e42
Павел Лиференко <eleven.krsk@gmail.com>
2020-10-31 13:30:04 +0200

    update server quick start.sh

--- server-quick-start.sh
+++ server-quick-start.sh
@@ -1,11 +1,10 @@
-# Install docker (clj + lein + psql) on EC2 aws instance
-sudo yum update -y ; \
-sudo amazon-linux-extras install -y docker ; \
-sudo service docker start ; \
-sudo usermod -a -G docker ec2-user ; \
-sudo yum install -y git ; \
-git clone https://github.com/Liferenko/test-crud-healthIT.git ; \
-cd test-crud-healthIT/cliniqBackend/ ; \
-cp .vimrc ../../.vimrc ; \
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install -y docker git
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+#sudo yum install -y git
+git clone https://github.com/Liferenko/test-crud-healthIT.git
+cd test-crud-healthIT/cliniqBackend/
+cp .vimrc ../../.vimrc
 sudo make run
-
